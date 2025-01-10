fd9a6782d7a96b45c7a6d878852b26da19d05c89
manuelpezzi <pezzimanuel@gmail.com>
2025-01-10 11:48:10 +0100

    guida condizioni

---
+++ index.js
@@ -0,0 +1,15 @@
+/*Consegna:
+Scrivi un programma che stampi i numeri da 1 a 100,
+ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+Prima di partire a scrivere codice poniamoci qualche domanda:
+Come faccio a sapere se un numero è divisibile per?
+Abbiamo visto qualcosa di particolare che possiamo usare?*/
+
+/*
+1-creare un ciclo da 0 a 100(preparazione)
+2-creare le condizioni dal più specifico al meno specifico(sviluppo)
+3-dare il valore ai divisibili per 3 frizz,dare il valore ai divisibili per 5 il buzz,dare il valore ai divisibili per entrambi frizzBuzz
+4-stampare(consegna)
+*/
+

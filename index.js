b6ef5e8995d0a001d46e64705467941e7ee2120a
manuelpezzi <pezzimanuel@gmail.com>
2025-01-10 12:14:02 +0100

    fine repo

--- index.js
+++ index.js
@@ -16,8 +16,15 @@ Abbiamo visto qualcosa di particolare che possiamo usare?*/
 for (let i = 0; i < 100; i++) {
 
     if (i % 3 == 0 && i % 5 == 0) {
-        console.log("frizzBuzz")
-    } if else (i % 5 == 0)
+        console.log("fizzBuzz")
+    } else if (i % 5 == 0) {
+        console.log("Buzz")
+    } else if (i % 3 == 0) {
+        console.log("fizz")
+    } else {
+        console.log(i)
+    }
+
 
 
 }
